ec808666177e0be9bf022a988da89acb021724d2
mardochee mawete <mardocheemawete@gmail>
2023-07-27 10:13:51 -0100

    modification array message

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
-let message = ['Bonjour mon ami.', 'continuez à jouer encore.', 'Vous avez intelligent vas y encore.', 'vous avez gagné le grand lot','Vous avez échoué'];
+let message = ['Bonjour mon ami.', 'Continuez à jouer encore.', 'Vous vous débrouillez bien, continuez encore.', 'vous avez gagné le grand lot','Vous avez échoué réessayer encore'];
 let indexTrouver= Math.floor(Math.random()*message.length);
 console.log(message[indexTrouver]);
