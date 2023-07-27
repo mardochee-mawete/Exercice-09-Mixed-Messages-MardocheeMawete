fd6c33440e1c1e051a0d346cb04c48ed8a9c8763
mardochee mawete <mardocheemawete@gmail>
2023-07-27 11:30:53 -0100

    search index array and display aleatory  message

--- index.js
+++ index.js
@@ -1,2 +1,3 @@
 let message = ['Bonjour mon ami.', 'continuez à jouer encore.', 'Vous avez intelligent vas y encore.', 'vous avez gagné le grand lot','Vous avez échoué'];
-
+let indexTrouver= Math.floor(Math.random()*message.length);
+console.log(message[indexTrouver]);
